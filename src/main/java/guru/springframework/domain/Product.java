c8ab705931157ed0076248cfc9691eb01c465552
Martin Alcubierre <martin.alcubierre@gmai.com>
2019-07-28 11:13:42 +0200

    Include lombok

--- src/main/java/guru/springframework/domain/Product.java
+++ src/main/java/guru/springframework/domain/Product.java
@@ -1,11 +1,13 @@
 package guru.springframework.domain;
 
 import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
 
 @Entity
+@Data
 public class Product {
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
@@ -23,51 +25,4 @@ public class Product {
     @ApiModelProperty(notes = "The price of the product", required = true)
     private BigDecimal price;
 
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public Integer getVersion() {
-        return version;
-    }
-
-    public void setVersion(Integer version) {
-        this.version = version;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getProductId() {
-        return productId;
-    }
-
-    public void setProductId(String productId) {
-        this.productId = productId;
-    }
-
-    public String getImageUrl() {
-        return imageUrl;
-    }
-
-    public void setImageUrl(String imageUrl) {
-        this.imageUrl = imageUrl;
-    }
-
-    public BigDecimal getPrice() {
-        return price;
-    }
-
-    public void setPrice(BigDecimal price) {
-        this.price = price;
-    }
 }
