3fd30b795a0eb3d18e550b40ffb785da83a8a2ec
Martin Alcubierre <martin.alcubierre@gmai.com>
2019-07-30 18:03:02 +0200

    Inused Imports

--- src/main/java/guru/springframework/config/SpringSecConfig.java
+++ src/main/java/guru/springframework/config/SpringSecConfig.java
@@ -1,11 +1,6 @@
 package guru.springframework.config;
 
-import javax.sql.DataSource;
-
-import org.flywaydb.core.Flyway;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
